9929694b395e7db2790f4c811e8c070ccd705ee6
ultranaco <alex@MXCDP4LP62.pricetravel.com.mx>
2020-08-16 13:41:50 -0500

    feat: Semaphore to control concurrency with tag name

---
+++ SemaphoreLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Falcon.Threading
+{
+  public class SemaphoreLock : IDisposable
+  {
+    public static ConcurrentDictionary<string, SemaphoreSlim> _semaphores = new ConcurrentDictionary<string, SemaphoreSlim>();
+
+    private SemaphoreSlim _semaphore;
+    private bool _raiseException;
+
+    public SemaphoreLock(string semaphoreName, int timeOut = 1000, int concurrency = 2, bool raiseException = false)
+    {
+      _raiseException = raiseException;
+      _semaphore = _semaphores.GetOrAdd(semaphoreName, (key) => { return new SemaphoreSlim(concurrency, concurrency); });
+      _semaphore.Wait(timeOut);
+    }
+
+    public void Dispose()
+    {
+      try
+      {
+        _semaphore.Release();
+      }
+      catch(Exception e)
+      {
+        if (_raiseException)
+          throw e;
+      }
+    }
+  }
+}
