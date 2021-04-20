7dfc4f1a071ec87f18cdfa0bd2852b92b52e7dba
ultranaco <alex@MXCDP4LP62.pricetravel.com.mx>
2021-04-20 01:54:29 -0500

    feat: add open/close async closure

--- SemaphoreLock.cs
+++ SemaphoreLock.cs
@@ -1,35 +1,84 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ultranaco.Threading
 {
   public class SemaphoreLock : IDisposable
   {
     public static ConcurrentDictionary<string, SemaphoreSlim> _semaphores = new ConcurrentDictionary<string, SemaphoreSlim>();
-    private SemaphoreSlim _semaphore;
-    private string Name;
 
-    /// <summary> SemaphoreLock lock execution by tag name and concurrent degree</summary>
-    /// <param name="semaphoreName">Used to tag many instances of semaphores</param>
-    /// <param name="concurrency">Number of executions allowed by semaphoreName</param>
-    public SemaphoreLock(string semaphoreName, int concurrency = 1, int timeOut = 5000)
+    private SemaphoreSlim _semaphore;
+    private string _name;
+    private int _concurrency;
+    private int _timeout;
+
+    public SemaphoreLock(string semaphoreName, int timeout = 1000, int concurrency = 2, bool open = false)
     {
-      this.Name = semaphoreName;
-      this._semaphore = _semaphores.GetOrAdd(semaphoreName, (key) => { return new SemaphoreSlim(concurrency, concurrency); });
-      _semaphore.Wait(timeOut);
-    } 
+      this._name = semaphoreName;
+      this._concurrency = concurrency;
+      this._timeout = timeout;
+
+      if (open)
+        this.Open();
+    }
+
+    public void Open(Action action)
+    {
+      Exception exception = null;
+
+      try
+      {
+        this.Open();
+        action();
+      }
+      catch (Exception e)
+      {
+        exception = e;
+      }
+      finally
+      {
+        this.Dispose();
+        if (exception != null)
+          throw exception;
+      }
+    }
+
+    public async Task OpenAsync(Func<Task> action)
+    {
+      Exception exception = null;
+
+      try
+      {
+        this.Open();
+        await action();
+      }
+      catch (Exception e)
+      {
+        exception = e;
+      }
+      finally
+      {
+        this.Dispose();
+        if (exception != null)
+          throw exception;
+      }
+    }
+
+    public void Open()
+    {
+      this._semaphore = _semaphores.GetOrAdd(this._name, (key) =>
+      {
+        return new SemaphoreSlim(this._concurrency, this._concurrency);
+      });
+
+      _semaphore.Wait(this._timeout);
+    }
 
     public void Dispose()
     {
-      try
-      {
-          _semaphore.Release();
-      }
-      catch
-      {
-        Console.WriteLine("Semaphore Counts: {0}", _semaphore.CurrentCount);
-      }
+      _semaphore.Release();
     }
   }
 }
